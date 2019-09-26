692e4cdeca8bb846f19ebb507ead71e9aa348852
LinLorry <lin864464995@163.com>
2019-09-26 21:07:21 +0800

    Add find User by id.

--- src/main/java/com/dghysc/hy/user/repo/UserRepository.java
+++ src/main/java/com/dghysc/hy/user/repo/UserRepository.java
@@ -3,9 +3,13 @@ package com.dghysc.hy.user.repo;
 import com.dghysc.hy.user.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User findUserById(Integer id);
+
     User findUserByUsername(String username);
 
-    User findUsersByName(String name);
+    List<User> findUsersByName(String name);
 
 }
