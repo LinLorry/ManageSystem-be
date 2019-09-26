cae40788523f2b8b8af2cf8bc690279608208893
LinLorry <lin864464995@163.com>
2019-09-26 20:05:51 +0800

    Update Registry.

--- src/main/java/com/dghysc/hy/user/UserController.java
+++ src/main/java/com/dghysc/hy/user/UserController.java
@@ -1,10 +1,12 @@
 package com.dghysc.hy.user;
 
+import com.alibaba.fastjson.JSONObject;
+import com.dghysc.hy.user.model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("/api/user")
@@ -13,9 +15,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ResponseBody
     @PostMapping("/registry")
-    public @ResponseBody
-    String registry() {
-        return "Hello World";
+    public JSONObject registry(@RequestBody User user) {
+        JSONObject result = new JSONObject();
+
+        if (userService.getUser(user.getUsername()) != null) {
+            result.put("status", 0);
+            result.put("message", "Username exist.");
+        } else if (userService.addUser(user.getUsername(), user.getName(), user.getPassword())) {
+            result.put("status", 1);
+            result.put("message", "registry success");
+        } else {
+            result.put("status", 0);
+            result.put("message", "registry failed");
+        }
+        return result;
     }
 }
