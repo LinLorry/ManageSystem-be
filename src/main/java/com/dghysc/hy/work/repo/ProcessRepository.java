14a28318a775d8b89caccc15ed4c232e3d9914b1
LinLorry <lin864464995@163.com>
2019-10-04 23:35:49 +0800

    Add Work, Process Repositories.

---
+++ src/main/java/com/dghysc/hy/work/repo/ProcessRepository.java
@@ -0,0 +1,8 @@
+package com.dghysc.hy.work.repo;
+
+import com.dghysc.hy.work.model.Process;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProcessRepository extends CrudRepository<Process, Integer> {
+
+}
---
+++ src/main/java/com/dghysc/hy/work/repo/WorkRepository.java
@@ -0,0 +1,8 @@
+package com.dghysc.hy.work.repo;
+
+import com.dghysc.hy.work.model.Work;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WorkRepository extends CrudRepository<Work, Integer> {
+
+}
