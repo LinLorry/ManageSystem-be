09e16e01e878ce0348d7c3b243e9f8d92b2470bf
LinLorry <lin864464995@163.com>
2019-10-05 00:00:34 +0800

    Add Work Service.

---
+++ src/main/java/com/dghysc/hy/work/WorkService.java
@@ -0,0 +1,51 @@
+package com.dghysc.hy.work;
+
+import com.dghysc.hy.work.model.Work;
+import com.dghysc.hy.work.repo.WorkRepository;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class WorkService {
+    private final WorkRepository workRepository;
+
+    public WorkService(WorkRepository workRepository) {
+        this.workRepository = workRepository;
+    }
+
+    boolean addWork(String name, String comment) {
+        Work work = new Work();
+
+        work.setName(name);
+        work.setComment(comment);
+
+        workRepository.save(work);
+
+        return true;
+    }
+
+    boolean updateWork(Integer id, String name, String comment) {
+        Optional<Work> optionalWork = workRepository.findById(id);
+        if (!optionalWork.isPresent()) {
+            return false;
+        }
+
+        Work work = optionalWork.get();
+        work.setName(name);
+        work.setComment(comment);
+        work.setUpdateDate(new Date(new java.util.Date().getTime()));
+
+        workRepository.save(work);
+        return true;
+    }
+
+    List<Work> getWorks() {
+        List<Work> works = new ArrayList<>();
+        for (Work work : workRepository.findAll()) {
+            works.add(work);
+        }
+        return works;
+    }
+}
