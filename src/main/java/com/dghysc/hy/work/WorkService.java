b18464c68946e8ab3b9a86bd7c5f59b59c638b91
LinLorry <lin864464995@163.com>
2019-10-05 12:37:05 +0800

    Update Work Service.

--- src/main/java/com/dghysc/hy/work/WorkService.java
+++ src/main/java/com/dghysc/hy/work/WorkService.java
@@ -2,12 +2,14 @@ package com.dghysc.hy.work;
 
 import com.dghysc.hy.work.model.Work;
 import com.dghysc.hy.work.repo.WorkRepository;
+import org.springframework.stereotype.Service;
 
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class WorkService {
     private final WorkRepository workRepository;
 
