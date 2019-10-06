1305280c9f7ea1299b7bbc11f4cf232cf9d91e02
LinLorry <lin864464995@163.com>
2019-10-06 23:27:32 +0800

    Add get Work api.

---
+++ src/main/java/com/dghysc/hy/work/WorkController.java
@@ -0,0 +1,71 @@
+package com.dghysc.hy.work;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/work")
+public class WorkController {
+    private final WorkService workService;
+
+    public WorkController(WorkService workService) {
+        this.workService = workService;
+    }
+
+    @ResponseBody
+    @PostMapping("/add")
+    public JSONObject add(@RequestBody JSONObject request) {
+        JSONObject result = new JSONObject();
+
+        if (workService.addWork(
+                request.getString("name"),
+                request.getString("comment")
+        )) {
+            result.put("status", 1);
+            result.put("message", "Add Work Success");
+        } else {
+            result.put("status", 0);
+            result.put("message", "Add Work failed");
+        }
+
+        return result;
+    }
+
+    @ResponseBody
+    @PostMapping("/update")
+    public JSONObject update(@RequestBody JSONObject request) {
+        JSONObject result = new JSONObject();
+
+        if (workService.updateWork(
+                request.getInteger("id"),
+                request.getString("name"),
+                request.getString("comment")
+        )) {
+            result.put("status", 1);
+            result.put("message", "Update Work Success");
+        } else {
+            result.put("status", 0);
+            result.put("message", "Update Work Failed");
+        }
+
+        return result;
+    }
+
+    @ResponseBody
+    @GetMapping("/get")
+    public JSONObject get(
+            @RequestParam(defaultValue = "1") Integer pageNumber,
+            @RequestParam(required = false) String name) {
+        JSONObject result = new JSONObject();
+
+        result.put("data", workService.getWorks(pageNumber, name));
+        result.put("status", 1);
+        result.put("message", "Get Works Success");
+
+        return result;
+    }
+
+
+
+
+}
