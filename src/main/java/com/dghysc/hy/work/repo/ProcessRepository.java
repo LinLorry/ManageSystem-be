81b1bdb43845f8a8cbd10ad0ea5ea6eec04d4385
LinLorry <lin864464995@163.com>
2019-10-06 23:46:15 +0800

    Update Process Repository.

--- src/main/java/com/dghysc/hy/work/repo/ProcessRepository.java
+++ src/main/java/com/dghysc/hy/work/repo/ProcessRepository.java
@@ -1,8 +1,13 @@
 package com.dghysc.hy.work.repo;
 
 import com.dghysc.hy.work.model.Process;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProcessRepository extends CrudRepository<Process, Integer> {
+    Page<Process> findAll(Pageable pageable);
+
+    Page<Process> findAllByNameContaining(Pageable pageable, String name);
 
 }
