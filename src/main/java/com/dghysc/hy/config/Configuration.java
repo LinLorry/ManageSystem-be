000547f3e8246b82ad140b481384d41154d7da3e
LinLorry <lin864464995@163.com>
2019-10-08 20:09:14 +0800

    Update Configuration.

--- src/main/java/com/dghysc/hy/config/Configuration.java
+++ src/main/java/com/dghysc/hy/config/Configuration.java
@@ -1,9 +1,8 @@
-package com.dghysc.hy;
+package com.dghysc.hy.config;
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration implements WebMvcConfigurer {
-
 
 }
