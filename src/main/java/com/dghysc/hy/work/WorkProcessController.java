2e3a94489f38c7ceafe493536f6b8a9266858c15
LinLorry <lin864464995@163.com>
2019-10-13 19:46:14 +0800

    Add Work Process Controller.

---
+++ src/main/java/com/dghysc/hy/work/WorkProcessController.java
@@ -0,0 +1,52 @@
+package com.dghysc.hy.work;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.dghysc.hy.work.model.WorkProcess;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/workProcess")
+public class WorkProcessController {
+    private final WorkProcessService workProcessService;
+
+    public WorkProcessController(WorkProcessService workProcessService) {
+        this.workProcessService = workProcessService;
+    }
+
+    @ResponseBody
+    @PostMapping("add")
+    public JSONObject add(@RequestBody JSONObject request) {
+        JSONObject response = new JSONObject();
+
+        JSONArray array = request.getJSONArray("data");
+        Object[] data = array.toArray();
+        Map<Integer, WorkProcess> map = new HashMap<>();
+
+        for (Object j : data) {
+            JSONObject t = (JSONObject) j;
+            map.put(t.getInteger("processId"),
+                    new WorkProcess(null, null, t.getInteger("sequenceNumber"))
+            );
+        }
+        if (workProcessService.addProcessesInWork(
+                request.getInteger("workId"),
+                map
+        )) {
+            response.put("status", 1);
+            response.put("message", "Add Process in Work Success");
+        } else {
+            response.put("status", 0);
+            response.put("message", "Add Process in Work Failed");
+        }
+
+        return response;
+    }
+}
