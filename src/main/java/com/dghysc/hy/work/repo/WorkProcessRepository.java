e399d6d97dd213d4ccfc5fb483becd964fc94897
LinLorry <lin864464995@163.com>
2019-12-01 13:15:19 +0800

    Add Work Process Repository.

---
+++ src/main/java/com/dghysc/hy/work/repo/WorkProcessRepository.java
@@ -0,0 +1,13 @@
+package com.dghysc.hy.work.repo;
+
+import com.dghysc.hy.work.model.WorkProcess;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Work Process Repository
+ * @author lorry
+ * @author lin864464995@163.com
+ * @see org.springframework.data.repository.CrudRepository
+ */
+public interface WorkProcessRepository extends CrudRepository<WorkProcess, Integer> {
+}
