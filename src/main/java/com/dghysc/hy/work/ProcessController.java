9be4831be733aa34f65a9039a0700cb219f384af
LinLorry <lin864464995@163.com>
2019-12-01 15:56:27 +0800

    Update process add and update api.

--- src/main/java/com/dghysc/hy/work/ProcessController.java
+++ src/main/java/com/dghysc/hy/work/ProcessController.java
@@ -1,10 +1,14 @@
 package com.dghysc.hy.work;
 
 import com.alibaba.fastjson.JSONObject;
+import com.dghysc.hy.until.SecurityUtil;
+import com.dghysc.hy.work.model.Process;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.NoSuchElementException;
 
 /**
@@ -23,58 +27,100 @@ public class ProcessController {
         this.processService = processService;
     }
 
+    /**
+     * Add Process Api
+     * @param request {
+     *     "name": process name: String[must],
+     *     "comment": process comment: String
+     * }
+     * @return add process success return {
+     *     "status": 1,
+     *     "message": "Add process success."
+     *     "data": {
+     *         "id": process id: Integer,
+     *         "name": process name: String,
+     *         "comment": process comment: String,
+     *         "createTime": process create time: Timestamp,
+     *         "updateTime": process update time: Timestamp
+     *     }
+     * }
+     */
     @ResponseBody
     @RequestMapping("/add")
     public JSONObject add(@RequestBody JSONObject request) {
         JSONObject response = new JSONObject();
 
+        String name = request.getString("name");
+        String comment = request.getString("comment");
+
+        Process process = new Process(name,
+                SecurityUtil.getUser(), new Timestamp(System.currentTimeMillis()));
+        process.setComment(comment);
+
         try {
-            response.put("processId", processService.addProcess(
-                    request.getString("name"),
-                    request.getString("comment")
-            ));
+            response.put("data", processService.add(process));
             response.put("status", 1);
-            response.put("message", "Add Process Success");
-        } catch (Exception e) {
-            logger.error(e);
-            response.put("status", 0);
-            if (processService.checkProcessByName(request.getString("name"))) {
-                response.put("message", "Process Name Exist.");
-            } else {
-                response.put("message", "Add Process Failed.");
+            response.put("message", "Add process success.");
+        } catch (DataIntegrityViolationException e) {
+            if (!processService.checkByName(name)) {
+                throw e;
             }
+
+            response.put("status", 0);
+            response.put("message", "Process name exist.");
         }
 
         return response;
     }
 
+    /**
+     * Update Process Api
+     * @param request {
+     *     "id": process id: Integer
+     *     "name": process name: String,
+     *     "comment": process comment: String
+     * }
+     * @return if update success return {
+     *     "status": 1,
+     *     "message": "Update process success."
+     *     "data": {
+     *         "id": process id: Integer,
+     *         "name": process name: String,
+     *         "comment": process comment: String,
+     *         "createTime": process create time: Timestamp,
+     *         "updateTime": process update time: Timestamp
+     *     }
+     * }
+     */
     @ResponseBody
     @RequestMapping("/update")
     public JSONObject update(@RequestBody JSONObject request) {
         JSONObject response = new JSONObject();
+        String name = request.getString("name");
+        String comment = request.getString("comment");
 
         try {
-            processService.updateProcess(
-                    request.getInteger("id"),
-                    request.getString("name"),
-                    request.getString("comment")
-            );
-            response.put("status", 1);
-            response.put("message", "Update Process Success");
-        } catch (NoSuchElementException e) {
-            logger.error(e);
-            response.put("status", 0);
-            response.put("message", "No such process");
-        } catch (Exception e) {
-            logger.error(e);
-            response.put("status", 0);
+            Process process = processService.loadById(request.getInteger("id"));
 
-            if (processService.checkProcessByName(request.getString("name"))) {
-                response.put("message", "Process Name Exist.");
-            } else {
-                response.put("message", "Update Process Failed");
+            process.setName(name);
+            process.setComment(comment);
+            process.setUpdateUser(SecurityUtil.getUser());
+            process.setUpdateTime(new Timestamp(System.currentTimeMillis()));
+
+            response.put("data", processService.update(process));
+            response.put("status", 1);
+            response.put("message", "Update process success.");
+        } catch (NoSuchElementException e) {
+            response.put("status", 0);
+            response.put("message", "No such process.");
+        } catch (DataIntegrityViolationException e) {
+            if (!processService.checkByName(name)) {
+                throw e;
             }
+            response.put("status", 0);
+            response.put("message", "Process name exist.");
         }
+
         return response;
     }
 
@@ -83,7 +129,7 @@ public class ProcessController {
     public JSONObject getProcesses(@RequestParam(defaultValue = "0") Integer pageNumber) {
         JSONObject response = new JSONObject();
 
-        response.put("data", processService.getProcesses(pageNumber));
+        response.put("data", processService.loadAll(pageNumber));
         response.put("status", 1);
         response.put("message", "Get Works Success");
 
@@ -96,7 +142,7 @@ public class ProcessController {
         JSONObject response = new JSONObject();
 
         try {
-            response.put("data", processService.loadProcess(id));
+            response.put("data", processService.loadById(id));
             response.put("status", 1);
             response.put("message", "Get Process Success");
         } catch (NoSuchElementException e) {
--- src/main/java/com/dghysc/hy/work/ProcessService.java
+++ src/main/java/com/dghysc/hy/work/ProcessService.java
@@ -5,10 +5,8 @@ import com.dghysc.hy.work.repo.ProcessRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 /**
  * Process Service
@@ -23,45 +21,24 @@ public class ProcessService {
         this.processRepository = processRepository;
     }
 
-    Integer addProcess(String name, String comment) {
-        Process process = new Process();
-
-        process.setName(name);
-        process.setComment(comment);
-        process.setUpdateTime(new Date(System.currentTimeMillis()));
-
-        processRepository.save(process);
-        return process.getId();
+    Process add(Process process) {
+        return processRepository.save(process);
     }
 
-    void updateProcess(Integer id, String name, String comment)
-            throws NoSuchElementException {
-        Optional<Process> optionalProcess = processRepository.findById(id);
-        if (!optionalProcess.isPresent()) {
-            throw new NoSuchElementException("There's no Process with id " + id);
-        }
-
-        Process process = optionalProcess.get();
-        process.setName(name);
-        process.setComment(comment);
-        process.setUpdateTime(new Date(System.currentTimeMillis()));
-
-        processRepository.save(process);
+    Process update(Process process) {
+        return processRepository.save(process);
     }
 
-    List<Process> getProcesses(Integer pageNumber) {
+    List<Process> loadAll(Integer pageNumber) {
         return processRepository.findAll(PageRequest.of(pageNumber, 20)).getContent();
     }
 
-    Process loadProcess(Integer id) throws NoSuchElementException {
-        Optional<Process> optionalProcess = processRepository.findById(id);
-        if (optionalProcess.isPresent()) {
-            return optionalProcess.get();
-        }
-        throw new NoSuchElementException("There's no Process with id " + id);
+    Process loadById(Integer id) throws NoSuchElementException {
+        return processRepository.findById(id)
+                .orElseThrow(NoSuchElementException::new);
     }
 
-    boolean checkProcessByName(String name) {
+    boolean checkByName(String name) {
         return processRepository.existsByName(name);
     }
 }
