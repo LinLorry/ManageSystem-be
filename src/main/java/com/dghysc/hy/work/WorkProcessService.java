c8059193c84fa318c6f382f25e6401a046a9e3a1
LinLorry <lin864464995@163.com>
2019-12-02 22:48:19 +0800

    Add load work process service.

--- src/main/java/com/dghysc/hy/work/WorkProcessService.java
+++ src/main/java/com/dghysc/hy/work/WorkProcessService.java
@@ -1,5 +1,6 @@
 package com.dghysc.hy.work;
 
+import com.alibaba.fastjson.JSONObject;
 import com.dghysc.hy.work.model.WorkProcess;
 import com.dghysc.hy.work.model.WorkProcessKey;
 import com.dghysc.hy.work.repo.WorkProcessRepository;
@@ -7,9 +8,18 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Work Process Service
  * @author lorry
@@ -47,6 +57,114 @@ public class WorkProcessService {
         session.close();
 
         return workProcess;
+    }
+
+    /**
+     * Load Work Process Service
+     * @param json {
+     *     "sequenceNumber": work process's sequence number equal this value,
+     *     "createTimeBefore": work process's create time before this value,
+     *     "updateTimeBefore": work process's update time before this value,
+     *     "createTimeAfter": work process's create time after this value,
+     *     "createTimeAfter": work process's update time after this value,
+     *     "work": {
+     *         "id": work process's work id equal this value,
+     *         "name": work process's work name contains this value,
+     *         "comment": work process's work comment contains this value,
+     *         "createTimeBefore": work process's work create time before this value,
+     *         "updateTimeBefore": work process's work update time before this value,
+     *         "createTimeAfter": work process's work create time after this value,
+     *         "createTimeAfter": work process's work update time after this value
+     *     },
+     *     "process": {
+     *         "id": work process's process id equal this value,
+     *         "name": work process's process name contains this value,
+     *         "comment": work process's process comment contains this value,
+     *         "createTimeBefore": work process's process create time before this value,
+     *         "updateTimeBefore": work process's process update time before this value,
+     *         "createTimeAfter": work process's process create time after this value,
+     *         "createTimeAfter": work process's process update time after this value
+     *     }
+     * }
+     * @return the load result.
+     */
+    List<WorkProcess> load(JSONObject json) {
+
+        Integer pageNumber = json.getInteger("pageNumber");
+        if (pageNumber == null) {
+            pageNumber = 0;
+        }
+
+        Specification<WorkProcess> specification = ((root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>(createPredicates(criteriaBuilder, json, root));
+            Integer sequenceNumber = json.getInteger("sequenceNumber");
+
+            if (sequenceNumber != null)
+                predicates.add(criteriaBuilder.equal(root.get("sequenceNumber"), sequenceNumber));
+
+
+            JSONObject work = json.getJSONObject("work");
+            JSONObject process = json.getJSONObject("process");
+
+            if (work != null)
+                predicates.addAll(createPredicates(criteriaBuilder, work, root.get("work")));
+
+            if (process != null)
+                predicates.addAll(createPredicates(criteriaBuilder, process, root.get("process")));
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[]{}));
+        });
+        return workProcessRepository.findAll(specification, PageRequest.of(pageNumber, 20)).getContent();
+    }
+
+    /**
+     * Create Predicates By Data
+     * @param criteriaBuilder the criteria builder.
+     * @param json {
+     *     "id": id equal this value,
+     *     "name": name contains this value,
+     *     "comment": comment contains this value,
+     *     "createTimeBefore": create time before this value,
+     *     "updateTimeBefore": update time before this value,
+     *     "createTimeAfter": create time after this value,
+     *     "createTimeAfter": update time after this value
+     * }
+     * @param path the path.
+     * @return the predicates.
+     */
+    private static List<Predicate> createPredicates(CriteriaBuilder criteriaBuilder, JSONObject json, Path<?> path) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        Integer id = json.getInteger("id");
+        String name = json.getString("name");
+        String comment = json.getString("comment");
+        Timestamp createTimeBefore = json.getTimestamp("createTimeBefore");
+        Timestamp updateTimeBefore = json.getTimestamp("updateTimeBefore");
+        Timestamp createTimeAfter = json.getTimestamp("createTimeAfter");
+        Timestamp updateTimeAfter = json.getTimestamp("updateTimeAfter");
+
+        if (id != null)
+            predicates.add(criteriaBuilder.equal(path.get("id"), id));
+
+        if (name != null && name.length() != 0)
+            predicates.add(criteriaBuilder.like(path.get("name"), "%" + name+ "%"));
+
+        if (comment != null && comment.length() != 0)
+            predicates.add(criteriaBuilder.like(path.get("comment"), "%" + comment + "%"));
+
+        if (createTimeBefore != null)
+            predicates.add(criteriaBuilder.lessThan(path.get("createTime"), createTimeBefore));
+
+        if (updateTimeBefore != null)
+            predicates.add(criteriaBuilder.lessThan(path.get("createTime"), updateTimeBefore));
+
+        if (createTimeAfter != null)
+            predicates.add(criteriaBuilder.greaterThan(path.get("createTime"), createTimeBefore));
+
+        if (updateTimeAfter != null)
+            predicates.add(criteriaBuilder.greaterThan(path.get("createTime"), updateTimeAfter));
+
+        return predicates;
     }
 
     /**
