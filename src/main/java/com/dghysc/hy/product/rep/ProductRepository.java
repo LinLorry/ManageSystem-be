29da2698c5d24f779bb8db726b08cbad78fa3570
LinLorry <lin864464995@163.com>
2019-12-05 20:20:44 +0800

    :sparkles: Add product repository.

---
+++ src/main/java/com/dghysc/hy/product/rep/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.dghysc.hy.product.rep;
+
+import com.dghysc.hy.product.model.Product;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Product Repository
+ * @author lorry
+ * @author lin864464995@163.com
+ * @see org.springframework.data.repository.CrudRepository
+ * @see org.springframework.data.jpa.repository.JpaSpecificationExecutor  .
+ */
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    boolean existsBySerial(String serial);
+}
