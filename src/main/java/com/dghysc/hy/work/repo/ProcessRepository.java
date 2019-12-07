061978430c1394126ab7042ddea1a57c19a2daf2
LinLorry <lin864464995@163.com>
2019-12-07 12:07:53 +0800

    :alien: Add delete work api and test.

--- src/main/java/com/dghysc/hy/work/repo/ProcessRepository.java
+++ src/main/java/com/dghysc/hy/work/repo/ProcessRepository.java
@@ -1,8 +1,6 @@
 package com.dghysc.hy.work.repo;
 
 import com.dghysc.hy.work.model.Process;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
