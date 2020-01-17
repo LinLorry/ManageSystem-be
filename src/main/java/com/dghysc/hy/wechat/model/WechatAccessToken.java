ea40abd7365f3c2207de61b1bd2bfc5479b6714e
LinLorry <lin864464995@163.com>
2020-01-18 01:35:14 +0800

    :tada: Add wechat Access token model.

---
+++ src/main/java/com/dghysc/hy/wechat/model/WechatAccessToken.java
@@ -0,0 +1,54 @@
+package com.dghysc.hy.wechat.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ * Wechat Access Token Model
+ * @author lorry
+ * @author lin864464995@163.com
+ */
+@Entity
+public class WechatAccessToken implements Serializable {
+    @Id
+    private String appId;
+
+    @Column(length = 512)
+    private String accessToken;
+
+    private Timestamp failureTime;
+
+    public WechatAccessToken() {
+    }
+
+    public WechatAccessToken(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Timestamp getFailureTime() {
+        return failureTime;
+    }
+
+    public void setFailureTime(Timestamp failureTime) {
+        this.failureTime = failureTime;
+    }
+}
