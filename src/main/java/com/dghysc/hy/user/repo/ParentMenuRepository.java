dc1547139170ef2b5b90459c21f59173dbbf4b4d
LinLorry <lin864464995@163.com>
2020-01-20 16:54:22 +0800

    :tada: Add parent menu repository.

---
+++ src/main/java/com/dghysc/hy/user/repo/ParentMenuRepository.java
@@ -0,0 +1,15 @@
+package com.dghysc.hy.user.repo;
+
+import com.dghysc.hy.user.model.ParentMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The Parent Menu Repository
+ * @author lorry
+ * @author lin864464995@163.com
+ * @see org.springframework.data.jpa.repository.JpaRepository
+ */
+@Repository
+public interface ParentMenuRepository extends JpaRepository<ParentMenu, Integer> {
+}
