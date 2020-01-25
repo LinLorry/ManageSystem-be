f15ee63a4ea90490e9018f4ecec563be4c1932ef
LinLorry <lin864464995@163.com>
2020-01-25 17:34:20 +0800

    :sparkles: Add findByIdAndIsDeleteFalse.

--- src/main/java/com/dghysc/hy/user/repo/RoleRepository.java
+++ src/main/java/com/dghysc/hy/user/repo/RoleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * The Role Repository
@@ -14,5 +15,8 @@ import java.util.List;
  */
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByIdAndIsDeleteFalse(Integer integer);
+
     List<Role> findAllByIsDeleteFalse();
 }
