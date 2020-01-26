cc76d0c76a0da0c8bf7961e210568f0363df66de
LinLorry <lin864464995@163.com>
2020-01-26 15:23:51 +0800

    :sparkles: Add exists by user id.

--- src/main/java/com/dghysc/hy/wechat/repo/WechatUserRepository.java
+++ src/main/java/com/dghysc/hy/wechat/repo/WechatUserRepository.java
@@ -1,6 +1,7 @@
 package com.dghysc.hy.wechat.repo;
 
 import com.dghysc.hy.wechat.model.WechatUser;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,10 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface WechatUserRepository extends CrudRepository<WechatUser, String> {
+    @Query(value = "SELECT " +
+            "CASE WHEN COUNT(wechatUser) > 0 " +
+            "THEN TRUE ELSE FALSE END " +
+            "FROM WechatUser wechatUser " +
+            "WHERE wechatUser.user.id = ?1")
+    Boolean existsByUserId(Long id);
 }
