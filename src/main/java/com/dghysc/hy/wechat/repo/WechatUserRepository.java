6435677e674f8880ed2058b7c1e0d81cffab9769
LinLorry <lin864464995@163.com>
2020-01-26 18:06:59 +0800

    :sparkles: Update wechat user repository to JpaRepository and add findAllByName.

--- src/main/java/com/dghysc/hy/wechat/repo/WechatUserRepository.java
+++ src/main/java/com/dghysc/hy/wechat/repo/WechatUserRepository.java
@@ -1,18 +1,23 @@
 package com.dghysc.hy.wechat.repo;
 
 import com.dghysc.hy.wechat.model.WechatUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Wechat User Repository
  * @author lorry
  * @author lin864464995@163.com
- * @see org.springframework.data.repository.CrudRepository
+ * @see org.springframework.data.jpa.repository.JpaRepository
  */
 @Repository
-public interface WechatUserRepository extends CrudRepository<WechatUser, String> {
+public interface WechatUserRepository extends JpaRepository<WechatUser, String> {
+
+    Page<WechatUser> findAllByName(String name, Pageable pageable);
+
     @Query(value = "SELECT " +
             "CASE WHEN COUNT(wechatUser) > 0 " +
             "THEN TRUE ELSE FALSE END " +
