8e58d8451b470db3a27565fe414a79ec1c42be05
LinLorry <lin864464995@163.com>
2020-02-25 22:28:08 +0800

    :white_check_mark: Update Work controller test.

--- src/test/java/com/dghysc/hy/work/WorkControllerTest.java
+++ src/test/java/com/dghysc/hy/work/WorkControllerTest.java
@@ -2,7 +2,9 @@ package com.dghysc.hy.work;
 
 import com.alibaba.fastjson.JSONObject;
 import com.dghysc.hy.util.TestUtil;
+import com.dghysc.hy.work.model.Process;
 import com.dghysc.hy.work.model.Work;
+import com.dghysc.hy.work.repo.ProcessRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,10 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -26,6 +32,9 @@ public class WorkControllerTest {
 
     @Autowired
     private TestUtil testUtil;
+
+    @Autowired
+    private ProcessRepository processRepository;
 
     @Test
     public void create() {
@@ -104,6 +113,49 @@ public class WorkControllerTest {
 
         ResponseEntity<JSONObject> responseEntity = restTemplate
                 .exchange(url, HttpMethod.DELETE, request, JSONObject.class);
+
+        JSONObject response = responseEntity.getBody();
+
+        System.out.println(response);
+        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertNotNull(response);
+        assertEquals(1, response.getIntValue("status"));
+    }
+
+    @Test
+    public void updateProcesses() {
+        final String url = baseUrl + "/processes";
+        JSONObject requestBody = new JSONObject();
+        Set<Integer> processIds = new HashSet<>();
+
+        while (processIds.size() != processRepository.count() && processIds.size() < 3) {
+            processIds.add(testUtil.nextId(Process.class));
+        }
+
+        requestBody.put("id", testUtil.nextId(Work.class));
+        requestBody.put("processes", new ArrayList<>(processIds));
+
+        HttpEntity<JSONObject> request = new HttpEntity<>(requestBody, testUtil.getTokenHeader());
+
+        ResponseEntity<JSONObject> responseEntity = restTemplate
+                .exchange(url, HttpMethod.POST, request, JSONObject.class);
+
+        JSONObject response = responseEntity.getBody();
+
+        System.out.println(response);
+        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertNotNull(response);
+        assertEquals(1, response.getIntValue("status"));
+    }
+
+    @Test
+    public void getProcesses() {
+        final String url = baseUrl + "/processes?id=" + testUtil.nextId(Work.class);
+
+        HttpEntity<JSONObject> request = new HttpEntity<>(testUtil.getTokenHeader());
+
+        ResponseEntity<JSONObject> responseEntity = restTemplate
+                .exchange(url, HttpMethod.GET, request, JSONObject.class);
 
         JSONObject response = responseEntity.getBody();
 
