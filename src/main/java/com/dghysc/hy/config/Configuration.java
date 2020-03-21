c464d32b58bfe18eae314d24c2c480fe70f4efd4
LinLorry <lin864464995@163.com>
2020-03-21 18:18:23 +0800

    :globe_with_meridians: Update use UTC time zone.

--- src/main/java/com/dghysc/hy/config/Configuration.java
+++ src/main/java/com/dghysc/hy/config/Configuration.java
@@ -2,7 +2,13 @@ package com.dghysc.hy.config;
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @org.springframework.context.annotation.Configuration
 public class Configuration implements WebMvcConfigurer {
-
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
