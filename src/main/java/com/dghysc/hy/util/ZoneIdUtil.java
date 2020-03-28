295e945c9ace5efbca3ce95e3704866a172ce040
LinLorry <lin864464995@163.com>
2020-03-28 23:38:20 +0800

    :bento: Add ZoneIdUtil.

---
+++ src/main/java/com/dghysc/hy/util/ZoneIdUtil.java
@@ -0,0 +1,11 @@
+package com.dghysc.hy.util;
+
+import java.time.ZoneId;
+
+public class ZoneIdUtil {
+
+    public static final ZoneId CST = ZoneId.of("GMT+8");
+
+    public static final ZoneId UTC = ZoneId.of("UTC");
+
+}
