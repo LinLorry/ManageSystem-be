c61ce532b0e1b81af598289ae68f5e30b4c5f46a
LinLorry <lin864464995@163.com>
2020-04-28 17:01:21 +0800

    :card_file_box: Update schedule message user model.

--- src/main/java/com/dghysc/hy/wechat/model/ScheduleMessageUser.java
+++ src/main/java/com/dghysc/hy/wechat/model/ScheduleMessageUser.java
@@ -1,7 +1,13 @@
 package com.dghysc.hy.wechat.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Optional;
 
 /**
  * Schedule Message User Model
@@ -9,6 +15,7 @@ import java.io.Serializable;
  * @author lin864464995@163.com
  */
 @Entity
+@Table(name = "schedule_message_user")
 public class ScheduleMessageUser implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -17,11 +24,28 @@ public class ScheduleMessageUser implements Serializable {
     @Column(nullable = false, updatable = false)
     private String id;  // wechat user open id.
 
+    @ManyToOne(optional = false, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "id", nullable = false,
+            insertable = false, updatable = false)
+    @NotFound(action = NotFoundAction.IGNORE)
+    @JsonIgnore
+    private WechatUser wechatUser;
+
+    public ScheduleMessageUser() { }
+
+    public ScheduleMessageUser(@NotNull String id) {
+        Optional.of(id).ifPresent(this::setId);
+    }
+
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public WechatUser getWechatUser() {
+        return wechatUser;
     }
 }
